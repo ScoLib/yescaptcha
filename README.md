f373ce1c3c35cfff906892c72f06aedfd63eaea7
klgd <slice1213@gmail.ccom>
2022-09-09 16:40:35 +0800

    1.1.0

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # yescaptcha
 
+http://www.yescaptcha.com/
+
 ## Install
 
 ```sh
@@ -14,11 +16,16 @@ Recaptcha,
 const Captcha = require("yescaptcha")
 
 // A new 'solver' instance with our API key
-const solver = new Captcha.Solver("<Your yescaptcha api key>")
+const solver = new Captcha.Solver("<Your yescaptcha api key>", {
+  pollingFrequency: 5000,
+  verbose: false,
+  baseUrl: 'https://api.yescaptcha.com', // https://china.yescaptcha.com
+})
 
 /* Example ReCaptcha Website */
-solver.recaptcha('websiteKey', 'websiteURL')
-.then((res) => {
+solver.recaptcha('websiteKey', 'websiteURL', {
+  type: 'NoCaptchaTaskProxyless', // more arg see https://yescaptcha.atlassian.net/wiki/spaces/YESCAPTCHA/overview?homepageId=33020
+}).then((res) => {
     console.log(res)
 })
 ```
--- index.js
+++ index.js
@@ -6,23 +6,24 @@ const { sleep } = require("./utils");
  * https://yescaptcha.atlassian.net/wiki/spaces/YESCAPTCHA/overview?homepageId=33020
  */
 class Solver {
-  constructor(apikey, pollingFrequency = 5000, verbose = false) {
+  constructor(apikey, options = {}) {
     this._apikey = apikey;
-    this._pollingFrequency = pollingFrequency;
-    this._verbose = verbose;
+    this._pollingFrequency = options.pollingFrequency || 5000;
+    this._verbose = options.verbose || false;
+    this._baseUrl = options.baseUrl || 'https://api.yescaptcha.com';
   }
 
   get apikey() { return this._apikey; }
   get pollingFrequency() { return this._pollingFrequency; }
   set apikey(update) { this._apikey = update; }
 
-  async pollResponse(taskId, pollingFreq = this.pollingFrequency) {
+  async pollResponse(taskId) {
     let data = {
       clientKey: this.apikey,
       taskId
     }
-    await sleep(pollingFreq);
-    const response = await axios.post('https://api.yescaptcha.com/getTaskResult', data)
+    await sleep(this.pollingFrequency);
+    const response = await axios.post(`${this._baseUrl}/getTaskResult`, data)
     // console.log(response)
 
     if (response.data.errorId == 0) {
@@ -32,7 +33,7 @@ class Solver {
           if (this._verbose) {
             console.log('task processing')
           }
-          return this.pollResponse(taskId, pollingFreq);
+          return this.pollResponse(taskId);
         default: {
           throw new Error(response.data.errorDescription);
         }
@@ -53,7 +54,7 @@ class Solver {
         ...args
       }
     }
-    const response = await axios.post('https://api.yescaptcha.com/createTask', data)
+    const response = await axios.post(`${this._baseUrl}/createTask`, data)
     // console.log(response)
     if (response.data.errorId == 0) {
       if (this._verbose) {
